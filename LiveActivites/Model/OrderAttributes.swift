5a6c93893ca1641357c557da76e6091439f8df9f
PapiaMCode <papiamcode@gmail.com>
2022-09-23 01:07:03 +0600

    Project setup

---
+++ LiveActivites/Model/OrderAttributes.swift
@@ -0,0 +1,17 @@
+//
+//  OrderAttributes.swift
+//  LiveActivites
+//
+//  Created by Jannatun Nahar Papia on 22/9/22.
+//
+
+import SwiftUI
+import ActivityKit
+
+struct OrderAttributes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+

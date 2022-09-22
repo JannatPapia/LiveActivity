043a90197b8b9a8ebcd7bd79f95b7127b42d317c
PapiaMCode <papiamcode@gmail.com>
2022-09-22 22:25:10 +0600

    Initial Commit

---
+++ LiveActivites/LiveActivitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivitesApp.swift
+//  LiveActivites
+//
+//  Created by Jannatun Nahar Papia on 22/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
